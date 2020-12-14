37e6f76a1e5dea6c71a8364836d20403e227efec
ressKim <sh21dlat@gmail.com>
2020-12-14 23:48:52 +0900

    kotlin study first day

--- src/main/kotlin/geometry/example/main.kt
+++ src/main/kotlin/geometry/example/main.kt
@@ -2,10 +2,13 @@ package geometry.example
 
 import geometry.shapes.Rectangle
 import geometry.shapes.createRandomRectangle
+import geometry.whenStudy.Color
 
 fun main(args: Array<String>) {
 
     println(createRandomRectangle().isSquared)
+
+    println(Color.BLUE.rgb())
 
 }
 
